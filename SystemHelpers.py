4c4cf052f558a58f4e783b3d29e89052d057217c
DucHoang <duc.hoang@globee.hk>
2021-11-13 00:01:45 +0700

    Okie

--- SystemHelpers.py
+++ SystemHelpers.py
@@ -4,18 +4,18 @@ import psutil
 
 def calculate_time_execute(callback):
     start_time = time.time()
-    if callback is not None and type(callback).__name__ == "function":
+    if callback is not None and type(callback).__name__ == 'function':
         callback()
     else:
-        raise TypeError("Callback is not function")
-    print(f"\n--- {(time.time() - start_time)} seconds ---")
+        raise TypeError('Callback is not function')
+    print(f'\n--- {(time.time() - start_time)} seconds ---')
 
 
 def get_interfaces():
-    # os.system("netsh interface show interface")
+    # os.system('netsh interface show interface')
     addrs = psutil.net_if_addrs()
     interfaces = [key for key in addrs.keys()]
     return interfaces
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     get_interfaces()
